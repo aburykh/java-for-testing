d9b6a6fb8e338515d400e1eff6ba0d903c12ca16
Aleksey Burykh <burykh@azurdrive.ru>
2020-05-21 23:12:38 +0300

    minor refactor hw

--- src/main/java/ru/geekbrains/java/for_testing/lesson3/Main.java
+++ src/main/java/ru/geekbrains/java/for_testing/lesson3/Main.java
@@ -19,28 +19,28 @@ public class Main {
 
     public static void main(String[] args) {
         try {
-            connect();
-            dropTable();
-//            clearTable();
-            createTableEx();
-            prepareStatements();
+            connect("jdbc:sqlite:jdbc.db");
+            dropTable("users");
+//            clearTable("users");
+            createTableEx("users","id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER, email VARCHAR");
+            prepareStatements("INSERT INTO users (name, age, email) VALUES (?, ?, ?);");
             psInsertEx();
 //            fillTableAndTransactionEx();
 
             System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>\nSelect * from users;\n");
-            readAll();
+            readAll("select * from users;");
 
             System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>\nDelete from users where name like 'bob2';\n");
-            deleteUserByName();
+            deleteUserByName("users","'bob2'");
             System.out.println("User has been deleted successfully\n");
 
             System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>\nSelect * from users where age between 20 and 45;\n");
-            showUsersByAge(20, 45);
+            showUsersByAge("users",20, 45);
 
             System.out.println("\n>>>>>>>>>>>>>>>>>>>>>>>>\nInsert into users (name, age, email) values ('bob5000','101','bobmail@123.ru');\n");
-            addUser();
+            addUser("users","'bob5000','101','bobmail@123.ru'");
             System.out.println("User has been added successfully:");
-            readNewUser();
+            readNewUser("users","where id >= 51");
 
         } catch (SQLException e) {
             e.printStackTrace();
@@ -50,8 +50,8 @@ public class Main {
 
     }
 
-    private static int clearTable() throws SQLException {
-        return statement.executeUpdate("DELETE FROM users;");
+    private static int clearTable(String tableName) throws SQLException {
+        return statement.executeUpdate("DELETE FROM " + tableName + ";");
     }
 
     private static void psInsertEx() throws SQLException {
@@ -66,12 +66,12 @@ public class Main {
     }
 
     // * 1. Создать таблицу с пользователями (users). У пользователя есть следующие поля: id, имя, возраст, email;
-    private static void createTableEx() throws SQLException {
-        statement.executeUpdate("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, age INTEGER, email VARCHAR);");
+    private static void createTableEx(String tableName, String parametersOfTable) throws SQLException {
+        statement.executeUpdate("CREATE TABLE IF NOT EXISTS " + tableName + " (" + parametersOfTable + ");");
     }
 
-    public static void prepareStatements() throws SQLException {
-        psInsert = connection.prepareStatement("INSERT INTO users (name, age, email) VALUES (?, ?, ?);");
+    public static void prepareStatements(String insertQuery) throws SQLException {
+        psInsert = connection.prepareStatement(insertQuery);
     }
 
     private static void fillTableAndTransactionEx() throws SQLException {
@@ -87,64 +87,58 @@ public class Main {
         System.out.println(System.currentTimeMillis() - time);
     }
 
-
-    private static void dropTable() throws SQLException {
-        statement.executeUpdate("DROP TABLE IF EXISTS users;");
+    private static void dropTable(String tableName) throws SQLException {
+        statement.executeUpdate("DROP TABLE IF EXISTS " + tableName + ";");
     }
 
 
-    // * 2. Написать метод, распечатывающий в консоль всю информацию обо всех пользователях из таблицы;
-    private static void readAll() throws SQLException {
-        try (ResultSet rs = statement.executeQuery("select * from users;")) {
+    private static void getMetaAndPrint(String query) throws SQLException {
+        try (ResultSet rs = statement.executeQuery(query)) {
             ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            System.out.printf(resultSetMetaData.getColumnName(1) + "\t" + resultSetMetaData.getColumnName(2) + "\t" + resultSetMetaData.getColumnName(3) + "\t" + resultSetMetaData.getColumnName(4) + "\n");
+            System.out.printf(resultSetMetaData.getColumnName(1)
+                    + "\t" + resultSetMetaData.getColumnName(2)
+                    + "\t" + resultSetMetaData.getColumnName(3)
+                    + "\t" + resultSetMetaData.getColumnName(4) + "\n");
 
             while (rs.next()) {
-                System.out.println(rs.getInt(1) + "\t" + rs.getString("name") + "\t" + rs.getInt(3) + "\t" + rs.getString("email"));
+                System.out.println(rs.getInt(1)
+                        + "\t" + rs.getString("name")
+                        + "\t" + rs.getInt(3)
+                        + "\t" + rs.getString("email"));
             }
         }
+    }
+
+    // * 2. Написать метод, распечатывающий в консоль всю информацию обо всех пользователях из таблицы;
+    private static void readAll(String query) throws SQLException {
+        getMetaAndPrint(query);
     }
 
     // * 3. Написать метод вывода в консоль пользователей, возраст которых находится в пределах min-max (public void showUsersByAge(int min, int max));
-    public static void showUsersByAge(int min, int max) throws SQLException {
-//    private static void readAll() throws SQLException {
-        try (ResultSet rs = statement.executeQuery("select * from users where age between 20 and 45;")) {
-            ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            System.out.printf(resultSetMetaData.getColumnName(1) + "\t" + resultSetMetaData.getColumnName(2) + "\t" + resultSetMetaData.getColumnName(3) + "\t" + resultSetMetaData.getColumnName(4) + "\n");
-
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + "\t" + rs.getString("name") + "\t" + rs.getInt(3) + "\t" + rs.getString("email"));
-            }
-        }
+    public static void showUsersByAge(String tableName, int min, int max) throws SQLException {
+        getMetaAndPrint("select * from " + tableName + " where age between " + min + " and " + max + ";");
     }
 
     // * 4. Написать метод удаления пользователя по имени;
     private static void
-    deleteUserByName() throws SQLException {
-        statement.executeUpdate("delete from users where name like 'bob2';");
+    deleteUserByName(String tableName, String name) throws SQLException {
+        statement.executeUpdate("delete from " + tableName + " where name like " + name + ";");
     }
 
     // * 5. Написать метод добавления пользователя в таблицу users из строки вида “имя возраст email”. Например, “Bob 32 bob32@gmail.com”.
-    private static void addUser() throws SQLException {
-        statement.executeUpdate("insert into users (name, age, email) values ('bob5000','101','bobmail@123.ru');");
+    private static void addUser(String tableName, String parameters) throws SQLException {
+        statement.executeUpdate("insert into " + tableName + " (name, age, email) values (" + parameters + ");");
     }
 
-    private static void readNewUser() throws SQLException {
-        try (ResultSet rs = statement.executeQuery("select * from users where id >= 51;")) {
-            ResultSetMetaData resultSetMetaData = rs.getMetaData();
-            System.out.printf(resultSetMetaData.getColumnName(1) + "\t" + resultSetMetaData.getColumnName(2) + "\t" + resultSetMetaData.getColumnName(3) + "\t" + resultSetMetaData.getColumnName(4) + "\n");
-
-            while (rs.next()) {
-                System.out.println(rs.getInt(1) + "\t" + rs.getString("name") + "\t" + rs.getInt(3) + "\t" + rs.getString("email"));
-            }
-        }
+    private static void readNewUser(String tableName, String conditions) throws SQLException {
+        getMetaAndPrint("select * from " + tableName + " " + conditions + ";");
     }
 
 
-    public static void connect() {
+    public static void connect(String dbUrl) {
         try {
             Class.forName("org.sqlite.JDBC");
-            connection = DriverManager.getConnection("jdbc:sqlite:jdbc.db");
+            connection = DriverManager.getConnection(dbUrl);
             statement = connection.createStatement();
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
