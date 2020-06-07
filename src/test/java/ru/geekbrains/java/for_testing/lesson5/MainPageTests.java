d59a1fe5e241cf67ab35d97eb506baa8af0b4323
Aleksey Burykh <burykh@azurdrive.ru>
2020-06-07 18:00:38 +0300

    uncommenting of tests

--- src/test/java/ru/geekbrains/java/for_testing/lesson5/MainPageTests.java
+++ src/test/java/ru/geekbrains/java/for_testing/lesson5/MainPageTests.java
@@ -1,16 +1,16 @@
-//package ru.geekbrains.java.for_testing.lesson5;
-//
-//import org.testng.Assert;
-//import org.testng.annotations.Test;
-//
-//public class MainPageTests extends BaseUITest {
-//
-//    @Test
-//    public void goToSignInPageTest() {
-//        mainPage.home();
-//        mainPage.goToSignIn();
-//        Assert.assertTrue(signInPage.isSignInPageHeaderPresent());
-//        Assert.assertEquals("authentication", signInPage.getSignInPageHeaderText().toLowerCase());
-//    }
-//
-//}
+package ru.geekbrains.java.for_testing.lesson5;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class MainPageTests extends BaseUITest {
+
+    @Test
+    public void goToSignInPageTest() {
+        mainPage.home();
+        mainPage.goToSignIn();
+        Assert.assertTrue(signInPage.isSignInPageHeaderPresent());
+        Assert.assertEquals("authentication", signInPage.getSignInPageHeaderText().toLowerCase());
+    }
+
+}
